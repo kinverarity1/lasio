84217a869caba1a9599f7dde45894fe11a974a7d
Kent Inverarity <kinverarity1@gmail.com>
2013-12-24 19:50:28 +1030

    backbone of package structure

---
+++ las_reader/__init__.py
@@ -0,0 +1,1 @@
+from _las_reader import *
---
+++ setup.py
@@ -0,0 +1,8 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(name='las_reader')
