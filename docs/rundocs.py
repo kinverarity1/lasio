788e9033029fd14a7e28cd2a61d609549e41e5df
Kent Inverarity <kinverarity1@gmail.com>
2014-04-06 12:49:14 +0930

    Update all docs

--- docs/rundocs.py
+++
@@ -1,8 +0,0 @@
-import glob, os
-
-from runipy.notebook_runner import NotebookRunner
-
-for fn in glob.glob('*.ipynb'):
-    if not fn.startswith('test'):
-        r = NotebookRunner(fn)
-        r.run_notebook()
