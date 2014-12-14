5acd2f1d6c29f340974710d063f4f80a252e706e
Kent Inverarity <kinverarity1@gmail.com>
2014-12-14 16:33:13 +1030

    Update code --
    - Add ability to write LAS files.
    - Minor updates and changes to internal bits

--- las_reader/las.py
+++ las_reader/las.py
@@ -24,6 +24,8 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
+WELL_REV_MNEMONICS = ['STRT', 'STOP', 'STEP', 'NULL']
+
 url_regexp = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}'
@@ -33,9 +35,9 @@ url_regexp = re.compile(
         r'(?::\d+)?' # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-Metadata = recordtype('Metadata', ['unit', 'value', 'descr'])
-Curve = recordtype('Curve', ['unit', 'API_code', 'descr'])
-Parameter = recordtype('Parameter', ['unit', 'value', 'descr'])
+Metadata = recordtype('Metadata', ['mnemonic', 'unit', 'value', 'descr'])
+Curve = recordtype('Curve', ['mnemonic', 'unit', 'API_code', 'descr', 'data', 'name'])
+Parameter = recordtype('Parameter', ['mnemonic', 'unit', 'value', 'descr'])
 
 DEFAULT_VALUES = {
     'version': {'VERS': '2.0',
@@ -49,6 +51,11 @@ DEFAULT_VALUES = {
 DEFAULT_ORDER = {
     'version': ['VERS', 'WRAP', 'DLM'],
 }
+
+VERS_FMT = '{mnemonic}.{unit} {value}: {descr}'
+WELL_FMT = '{mnemonic}.{unit} {descr}: {value}'
+CURV_FMT = '{mnemonic}.{unit} {API_code} : {descr}'
+PARM_FMT = '{mnemonic}.{unit} {value} : {descr}'
 
 
 class OrderedDictionary(collections.OrderedDict):
@@ -66,6 +73,7 @@ class OrderedDictionary(collections.OrderedDict):
             return dict([(k, v.value) for k, v in self.items()])
         else:
             return dict([(k, v.descr) for k, v in self.items()])
+
 
 
 def open_file(file_obj, **kwargs):
@@ -103,12 +111,21 @@ class Las(OrderedDictionary):
         - *file*: open file object or filename
 
     '''
-
     def __init__(self, file=None, create=None, **kwargs):
         OrderedDictionary.__init__(self)
-        if not file is None:
+        self.provenance = None
+        self._text = ''
+        self.version = {}
+        self.well = {}
+        self.curves = []
+        self.params = {}
+
+        self.other = ''
+        # self.data = {}
+
+        if not (file is None):
             self.read(file, **kwargs)
-        elif not create is None:
+        elif not (create is None):
             self.create(create, **kwargs)
         
     def read(self, file, **kwargs):
@@ -124,22 +141,146 @@ class Las(OrderedDictionary):
         reader.wrap = self.version['WRAP'].value == 'YES'
 
         self.well = reader.read_section('~W')
-        self.curves = reader.read_section('~C')
+        self.curves = reader.read_list_section('~C')
         self.params = reader.read_section('~P')
         self.other = reader.read_raw_text('~O')
 
         # Set null value
-        reader.null = self.well['NULL']
+        reader.null = self.well['NULL'].value
 
-        self.data = reader.read_data(len(self.curves))
+        data = reader.read_data(len(self.curves))
 
-        n = len(self.curves.keys())
-        for i, (key, value) in enumerate(self.curves.iteritems()):
-            d = self.data[:, i]
-            self[key] = d
+        n = len(self.curves)
+        curve_names = [c.name for c in self.curves]
+        curve_freq = {}
+        curve_count = {}
+        for curve_name in curve_names:
+            if not curve_name in curve_freq:
+                curve_freq[curve_name] = 1
+            else:
+                curve_freq[curve_name] += 1
+            curve_count[curve_name] = 0
+        for i, c in enumerate(self.curves):
+            curve_count[c.mnemonic] += 1
+            if curve_freq[c.mnemonic] > 1:
+                c.name += '[%d]' % curve_count[c.mnemonic]
+
+        for i, c in enumerate(self.curves):
+            d = data[:, i]
+            c.data = d
+            if c.mnemonic in self.keys():
+                logger.warning('Multiple curves with the same mnemonic (%s).' % c.mnemonic)
+                self[c.name] = d
+            else:
+                self[c.name] = d
             self[i] = d
             self[i - n] = d
 
+    @property
+    def data(self):
+        return numpy.vstack([c.data for c in self.curves]).T
+
+    def write(self, file):
+        lines = []
+
+        # Write Version section
+        self.version['VERS'] = Metadata('VERS', '', 1.2, 'Version of LAS file format')
+        self.version['WRAP'] = Metadata('WRAP', '', 'NO', 'Is data wrapped across line breaks?')
+        lines.append('~Version '.ljust(60, '-'))
+        l_mnem = 0
+        l_value = 0
+        for vm in self.version.values():
+            if len(vm.mnemonic) > l_mnem:
+                l_mnem = len(vm.mnemonic)
+            if len(str(vm.value)) > l_value:
+                l_value = len(str(vm.value))
+        for vm in self.version.values():
+            vm_d = vm.todict()
+            vm_d['mnemonic'] = vm_d['mnemonic'].rjust(l_mnem)
+            vm_d['value'] = str(vm_d['value']).rjust(l_value)
+            lines.append(VERS_FMT.format(**vm_d))
+
+        # Write Well section
+        self.well['NULL'] = Metadata('NULL', '', -999.25, '')
+        lines.append('~Well '.ljust(60, '-'))
+        l_left = 0
+        left_rev = lambda rt: '{mnemonic}.{unit} {value}'.format(**rt.todict())
+        left_norm = lambda rt: '{mnemonic}.{unit} {descr}'.format(**rt.todict())
+        for wm in self.well.values():
+            if wm.mnemonic in WELL_REV_MNEMONICS:
+                s_left = left_rev(wm)
+            else:
+                s_left = left_norm(wm)
+            if len(s_left) > l_left:
+                l_left = len(s_left)
+        for wm in self.well.values():
+            wm_leftmost = '{mnemonic}.{unit}'.format(**wm.todict())
+            if wm.mnemonic in WELL_REV_MNEMONICS:
+                wm_left = wm_leftmost + str(wm.value).rjust(l_left - len(wm_leftmost))
+                lines.append(wm_left + ': ' + wm.descr)
+            else:
+                wm_left = wm_leftmost + str(wm.descr).rjust(l_left - len(wm_leftmost))
+                lines.append(wm_left + ': ' + wm.value)
+
+        # Write Curves section
+        lines.append('~Curves '.ljust(60, '-'))
+        l_mnem_unit = 0
+        l_API_code = 0
+        for cm in self.curves:
+            s_mnem_unit = '{mnemonic}.{unit}'.format(**cm.todict())
+            if len(s_mnem_unit) > l_mnem_unit:
+                l_mnem_unit = len(s_mnem_unit)
+            s_API_code = str(cm.API_code)
+            if len(s_API_code) > l_API_code:
+                l_API_code = len(s_API_code)
+        for cm in self.curves:
+            s_left = '{mnemonic}.{unit}'.format(**cm.todict()).ljust(l_mnem_unit)
+            s_right = str(cm.API_code).rjust(l_API_code)
+            lines.append(s_left + ' ' + s_right + ': ' + cm.descr)
+
+        # Write Params section
+        lines.append('~Parameters '.ljust(60, '-'))
+        l_mnem_unit = 0
+        l_value = 0
+        for pm in self.params.values():
+            s_mnem_unit = '{mnemonic}.{unit}'.format(**pm.todict())
+            if len(s_mnem_unit) > l_mnem_unit:
+                l_mnem_unit = len(s_mnem_unit)
+            s_value = str(pm.value)
+            if len(s_value) > l_value:
+                l_value = len(s_value)
+        for pm in self.params.values():
+            s_left = '{mnemonic}.{unit}'.format(**pm.todict()).ljust(l_mnem_unit)
+            s_right = str(pm.value).rjust(l_value)
+            lines.append(s_left + ' ' + s_right + ': ' + pm.descr)
+
+        # Write Other section
+        lines.append('~Other '.ljust(60, '-'))
+        lines += self.other.split('\n')
+
+        # Write Data section
+        lines.append('~ASCII Data '.ljust(60, '-'))
+        file.write('\n'.join(lines))
+        file.write('\n')
+        data_arr = numpy.column_stack([c.data for c in self.curves])
+        nrows, ncols = data_arr.shape
+        # FORMAT %10.5g
+        def fmt(n, fmt='%10.5g', l=10, spacer=' '):
+            if numpy.isnan(n):
+                return spacer + str(self.well['NULL'].value).rjust(l)
+            else:
+                return spacer + (fmt % n).rjust(l)
+        for i in range(nrows):
+            line = ''
+            for j in range(ncols):
+                line += fmt(data_arr[i, j])
+            file.write(line + '\n')
+        # numpy.savetxt(file, data_arr, fmt='%10.5g')
+
+    def get_curve_name(self, curve_name):
+        for curve in self.curves:
+            if curve.name == curve_name:
+                return curve
 
     def keys(self):
         k = super(OrderedDictionary, self).keys()
@@ -177,8 +318,11 @@ class Las(OrderedDictionary):
     @metadata.setter
     def metadata(self, value):
         raise Warning('Set values in the version/well/params attributes directly')
-    
 
+    @property
+    def index(self):
+        return self.data[:, 0]
+    
 
 
 class Reader(object):
@@ -202,11 +346,13 @@ class Reader(object):
         return names
 
 
-    def iter_section_lines(self, section_name):
+    def iter_section_lines(self, section_name, ignore_comments=True):
         in_section = False
         for line in self.lines:
             line = line.strip().strip('\t').strip()
-            if not line or line.startswith('#'):
+            if not line:
+                continue
+            if ignore_comments and line.startswith('#'):
                 continue
             if line.startswith(section_name):
                 in_section = True
@@ -219,14 +365,12 @@ class Reader(object):
         
 
     def read_raw_text(self, section_name):
-        return '\n'.join(self.iter_section_lines(section_name))
+        return '\n'.join(self.iter_section_lines(section_name, ignore_comments=False))
         
 
     def read_section(self, section_name):
-        is_section = lambda txt: section_name.startswith(txt)
         parser = SectionParser(section_name, version=self.version)
         d = OrderedDictionary()
-        in_section = False
         for line in self.iter_section_lines(section_name):      
             try:
                 values = read_line(line)
@@ -236,7 +380,19 @@ class Reader(object):
             else:
                 d[values['name']] = parser(**values)
         return d
-    
+
+    def read_list_section(self, section_name):
+        parser = SectionParser(section_name, version=self.version)
+        l = []
+        for line in self.iter_section_lines(section_name):      
+            try:
+                values = read_line(line)
+            except:
+                print('Failed to read in NAME.UNIT VALUE:DESCR'
+                      ' from:\n\t%s' % line)
+            else:
+                l.append(parser(**values))
+        return l
 
     def read_data(self, number_of_curves=None):
         s = self.read_data_string()
@@ -245,20 +401,21 @@ class Reader(object):
         else:
             s = s.replace('\n', ' ').replace('\t', ' ')
             arr = numpy.loadtxt(StringIO.StringIO(s))
-            print('arr shape = %s' % (arr.shape))
-            print('number of curves = %s' % number_of_curves)
+            logger.debug('arr shape = %s' % (arr.shape))
+            logger.debug('number of curves = %s' % number_of_curves)
             arr = numpy.reshape(arr, (-1, number_of_curves))
         if not arr.shape or (arr.ndim == 1 and arr.shape[0] == 0):
             logger.warning('No data present.')
             return None, None
         else:
             logger.info('Las file shape = %s' % str(arr.shape))
-        df_dict = {}
+        logger.debug('checking for nulls (NULL = %s)' % self.null)
         arr[arr == self.null] = numpy.nan
         return arr
         
 
     def read_data_string(self):
+        start_data = None
         for i, line in enumerate(self.lines):
             line = line.strip().strip('\t').strip()
             if line.startswith('~A'):
@@ -304,29 +461,28 @@ class SectionParser(object):
 
     def metadata(self, **keys):
         if self.version < 2:
-            if (keys['name'] in ['STRT', 'STOP', 'STEP', 'NULL']
-                or self.section_name.startswith('~V')):
-                return Metadata(keys['unit'], self.num(keys['value']), keys['descr'])
+            if (keys['name'] in WELL_REV_MNEMONICS or self.section_name.startswith('~V')):
+                return Metadata(keys['name'], keys['unit'], self.num(keys['value']), keys['descr'])
             else:
-                return Metadata(keys['unit'], self.num(keys['descr']), keys['value'])
+                return Metadata(keys['name'], keys['unit'], self.num(keys['descr']), keys['value'])
         else:
-            return Metadata(keys['unit'], self.num(keys['value']), keys['descr'])
+            return Metadata(keys['name'], keys['unit'], self.num(keys['value']), keys['descr'])
 
 
     def curves(self, **keys):
-        return Curve(keys['unit'], keys['value'], keys['descr'])
+        return Curve(keys['name'], keys['unit'], keys['value'], keys['descr'], None, keys['name'])
 
 
     def params(self, **keys):
-        return Parameter(keys['unit'], self.num(keys['value']), keys['descr'])
+        return Parameter(keys['name'], keys['unit'], self.num(keys['value']), keys['descr'])
 
 
 
 def read_line(line):
-    d = {'name': None,
-         'unit': None,
-         'value': None,
-         'descr': None}
+    d = {'name': '',
+         'unit': '',
+         'value': '',
+         'descr': ''}
     if ':' in line and '.' in line.split(':', 1)[0]:
         split_period = line.split('.')
         d['name'] = split_period[0].strip()
@@ -353,24 +509,4 @@ def read_line(line):
   
 
 
-class ExcelConverter(object):
-    def __init__(self, las_obj):
-        self.las = las_obj
-        
-    def write_excel(self, xlsfn):
-        import xlwt
-        wb = xlwt.Workbook()
-        md_sheet = wb.add_sheet('Metadata')
-        curves_sheet = wb.add_sheet('Curves')
-        
-        for i, (key, value) in enumerate(self.las.metadata.items()):
-            md_sheet.write(i, 0, key)
-            md_sheet.write(i, 1, value)
-            
-        for i, (name, data) in enumerate(self.las.items()):
-            curves_sheet.write(0, i, name)
-            for j, value in enumerate(data):
-                curves_sheet.write(j + 1, i, value)
-        
-        wb.save(xlsfn)
 
