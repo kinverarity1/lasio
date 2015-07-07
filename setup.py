49fb3e36aaf20af804ce7745c75c88e096d133ab
VelizarVESSELINOV <VV@slb163185007151.calgary.oilfield.slb.com>
2015-07-07 09:48:26 -0700

    Cleaning bad copy and paste

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from distutils.core import setup
 
 setup(name='las_reader',
-      version='0.9.0',
+      #version='0.9.0',
 
       description='LAS reader',
-      long_description='Python package to read Log ASCII Standard files that was started by https://github.com/kinverarity1',
+      #long_description='',
 
       # The project's main homepage.
-      #url='https://github.com/VelizarVESSELINOV/las-reader',
 
       # Author details
-      #author='Velizar VESSELINOV after fork of https://github.com/kinverarity1',
-      #author_email='Velizar.VESSELINOV@gmail.com',
+      #author='',
+      #author_email='',
       #install_requires=['numpy>1.8' , 'pandas>0.0', 'namedlist>1.6',],
 
       # Choose your license
-      license='BSD',
+      #license='',
 
       # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -23,29 +22,29 @@ setup(name='las_reader',
             #   3 - Alpha
             #   4 - Beta
             #   5 - Production/Stable
-            'Development Status :: 3 - Beta',
+            #'Development Status :: 3 - Beta',
 
             # Indicate who your project is intended for
-            'Intended Audience :: Intended Audience :: Customer Service',
-            'Intended Audience :: Developers',
-            'Intended Audience :: End Users/Desktop',
-            'Intended Audience :: Science/Research',
-            'Environment :: Console',
-            'Operating System :: OS Independent',
-            'Topic :: System :: Filesystems',
-            'Topic :: Scientific/Engineering :: Information Analysis',
+            #'Intended Audience :: Intended Audience :: Customer Service',
+            #'Intended Audience :: Developers',
+            #'Intended Audience :: End Users/Desktop',
+            #'Intended Audience :: Science/Research',
+            #'Environment :: Console',
+            #'Operating System :: OS Independent',
+            #'Topic :: System :: Filesystems',
+            #'Topic :: Scientific/Engineering :: Information Analysis',
 
             # Pick your license as you wish (should match "license" above)
-            'License :: OSI Approved :: BSD License',
+            #'License :: OSI Approved :: BSD License',
 
             # Specify the Python versions you support here. In particular, ensure
             # that you indicate whether you support Python 2, Python 3 or both.
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
+            #'Programming Language :: Python :: 2',
+            #'Programming Language :: Python :: 2.6',
+            #'Programming Language :: Python :: 2.7',
+            #'Programming Language :: Python :: 3',
+            #'Programming Language :: Python :: 3.2',
+            #'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
       ],
       packages=["las_reader"],
