e9e973b14e5672405be4961112ae8d66ba0ad4dc
Kent Inverarity <kinverarity1+github@gmail.com>
2015-07-08 12:06:03 +0930

    update setup.py according to PPUG
    I am following the "Python Packaging User Guide" at
    https://packaging.python.org/en/latest/distributing.html

--- setup.py
+++ setup.py
@@ -1,53 +1,63 @@
+"""Setup script for las_reader
+
+Based on:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
 from distutils.core import setup
 
+
 setup(name='las_reader',
-      # packages=["las_reader",],
-      entry_points={'console_scripts': ['las2excel = las_reader.las2excel:main']}
-      #version='0.9.0',
 
-      #description='LAS reader',
-      #long_description='',
+      version="0.2",
+      
+      description="Read geophysical log standard ASCII files",
+      long_description=("A library for reading data from log standard ASCII "
+                        "(LAS) files -- only versions 1.2 and 2.0 are currently"
+                        " supported."),
+      
+      url="https://github.com/kinverarity1/las_reader",
+      
+      author="Kent Inverarity",
+      author_email="kinverarity1@gmail.com",
+      
+      license="MIT",
+      
+      classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: Customer Service",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: Other Audience",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2 :: Only",
+            "Topic :: Scientific/Engineering",
+            "Topic :: System :: Filesystems",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            ],
 
-      # The project's main homepage.
-
-      # Author details
-      #author='',
-      #author_email='',
-      #install_requires=['numpy>1.8' , 'pandas>0.0', 'namedlist>1.6',],
-
-      # Choose your license
-      #license='',
-
-      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-      #classifiers=[
-            # How mature is this project? Common values are
-            #   3 - Alpha
-            #   4 - Beta
-            #   5 - Production/Stable
-            #'Development Status :: 3 - Beta',
-
-            # Indicate who your project is intended for
-            #'Intended Audience :: Intended Audience :: Customer Service',
-            #'Intended Audience :: Developers',
-            #'Intended Audience :: End Users/Desktop',
-            #'Intended Audience :: Science/Research',
-            #'Environment :: Console',
-            #'Operating System :: OS Independent',
-            #'Topic :: System :: Filesystems',
-            #'Topic :: Scientific/Engineering :: Information Analysis',
-
-            # Pick your license as you wish (should match "license" above)
-            #'License :: OSI Approved :: BSD License',
-
-            # Specify the Python versions you support here. In particular, ensure
-            # that you indicate whether you support Python 2, Python 3 or both.
-            #'Programming Language :: Python :: 2',
-            #'Programming Language :: Python :: 2.6',
-            #'Programming Language :: Python :: 2.7',
-            #'Programming Language :: Python :: 3',
-            #'Programming Language :: Python :: 3.2',
-            #'Programming Language :: Python :: 3.3',
-            #'Programming Language :: Python :: 3.4',
-      #],
-      #packages=["las_reader"],
+      keywords="science geophysics io",
+      
+      packages=["las_reader", ],
+      
+      install_requires=["numpy", "namedlist", ],
+      
+      entry_points={
+            'console_scripts': [
+                  'las2excel = las_reader.las2excel:main'
+            ],
+      }
 )
