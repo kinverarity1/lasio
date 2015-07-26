54400c6d15d571a80eb099a87428b7566e4d50a6
Kent Inverarity <kinverarity1@gmail.com>
2015-07-26 13:36:00 +0930

    run PEP8 check

--- setup.py
+++ setup.py
@@ -18,47 +18,47 @@ from distutils.core import setup
 setup(name='las_reader',
 
       version="0.3",
-      
+
       description="Read borehole data from Log ASCII Standard (LAS) files",
       long_description=("A library for reading borehole data from log "
                         "standard ASCII (LAS) files (only specifications "
                         "1.2 and 2.0 are supported for now)."),
-      
+
       url="https://github.com/kinverarity1/las_reader",
-      
+
       author="Kent Inverarity",
       author_email="kinverarity1@gmail.com",
-      
+
       license="MIT",
-      
+
       classifiers=[
-            "Development Status :: 4 - Beta",
-            "Environment :: Console",
-            "Intended Audience :: Customer Service",
-            "Intended Audience :: Developers",
-            "Intended Audience :: Education",
-            "Intended Audience :: End Users/Desktop",
-            "Intended Audience :: Other Audience",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: MIT License",
-            "Natural Language :: English",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Topic :: Scientific/Engineering",
-            "Topic :: System :: Filesystems",
-            "Topic :: Scientific/Engineering :: Information Analysis",
-            ],
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "Intended Audience :: Customer Service",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Education",
+          "Intended Audience :: End Users/Desktop",
+          "Intended Audience :: Other Audience",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Topic :: Scientific/Engineering",
+          "Topic :: System :: Filesystems",
+          "Topic :: Scientific/Engineering :: Information Analysis",
+      ],
 
       keywords="science geophysics io",
-      
+
       packages=["las_reader", ],
-      
+
       install_requires=["numpy", "namedlist", ],
-      
+
       entry_points={
-            'console_scripts': [
-                  'las2excel = las_reader.las2excel:main'
-            ],
+          'console_scripts': [
+              'las2excel = las_reader.las2excel:main'
+          ],
       }
-)
+      )
