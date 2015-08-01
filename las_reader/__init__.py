437f6c6ac5c347129f07a353ba36e12d02c49e30
Kent Inverarity <kinverarity1@gmail.com>
2015-08-01 09:47:36 +0930

    working on Excel classes

--- las_reader/__init__.py
+++ las_reader/__init__.py
@@ -5,6 +5,24 @@ from las import __version__
 
 def read(file_ref, encoding=None,
          autodetect_encoding=False, autodetect_encoding_chars=20000):
+    '''Read a LAS file.
+
+        Args:
+          file_ref: either a filename, an open file object, or a string of
+            a LAS file contents.
+
+        Kwargs:
+          encoding (str): character encoding to open file_ref with
+          autodetect_encoding (bool): use chardet/ccharet to detect encoding
+          autodetect_encoding_chars (int/None): number of chars to read from LAS
+            file for auto-detection of encoding.
+
+    Returns: a las.LASFile object
+
+    Note that it only supports versions 1.2 and 2.0 of the LAS file
+    specification.
+
+    '''
     return las.LASFile(file_ref, encoding=encoding,
                        autodetect_encoding=autodetect_encoding,
                        autodetect_encoding_chars=autodetect_encoding_chars)
--- las_reader/las2excel.py
+++ las_reader/las2excel.py
@@ -3,15 +3,52 @@ try:
 except ImportError:
     argparse = None
 import sys
-    
+
 from . import las
+
+
+class ExcelConverter(object):
+
+    def __init__(self, las):
+        self.las = las
+
+    def write_excel(self, xlsfn):
+        try:
+            import pandas
+            self.write_excel_pandas(self, xlsfn)
+        except ImportError:
+            try:
+                import xlwt
+                self.write_excel_xlwt(self, xlsfn)
+            except ImportError:
+                raise ImportError("Either pandas or xlwt are "
+                                  "required to create Excel files")
+
+    def write_excel_pandas(self):
+        pass
+
+    def write_excel_xlwt(self):
+        wb = xlwt.Workbook()
+        md_sheet = wb.add_sheet('Metadata')
+        curves_sheet = wb.add_sheet('Curves')
+
+        for i, (key, value) in enumerate(self.las.metadata_list()):
+            md_sheet.write(i, 0, key)
+            md_sheet.write(i, 1, value)
+
+        for i, curve in enumerate(self.las.curves):
+            curves_sheet.write(0, i, curve.name)
+            for j, value in enumerate(curve.data):
+                curves_sheet.write(j + 1, i, value)
+
+        wb.save(xlsfn)
 
 
 def main():
     if argparse:
         args = get_parser().parse_args(sys.argv[1:])
-        lasfn = args.las_filename
-        xlsfn = args.xls_filename
+        lasfn = args.LAS_filename
+        xlsfn = args.Excel_filename
     else:
         if len(sys.argv >= 3):
             lasfn = sys.argv[1]
@@ -21,18 +58,18 @@ def main():
                   'Usage:\n\n'
                   'las2excel.py example.las output.xls')
             sys.exit(1)
-        
-    l = las.Las(lasfn)
-    converter = core.ExcelConverter(l)
+
+    l = las.LASFile(lasfn)
+    converter = ExcelConverter(l)
     converter.write_excel(xlsfn)
 
-    
+
 def get_parser():
     parser = argparse.ArgumentParser('Convert LAS file to Excel')
-    parser.add_argument('las_filename')
-    parser.add_argument('xls_filename')
+    parser.add_argument('LAS_filename')
+    parser.add_argument('Excel_filename')
     return parser
-    
-    
+
+
 if __name__ == '__main__':
     main()
