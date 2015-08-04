9fa2a2d0e6e2f35553f6af7848d9b7fd679aed00
Kent Inverarity <kinverarity1@gmail.com>
2015-08-04 18:19:00 +0930

    Add test case for #42 (failing tests re missing mnemonics)

--- tests/test_read.py
+++ tests/test_read.py
@@ -5,7 +5,36 @@ import las_reader
 
 test_dir = os.path.dirname(__file__)
 
+egfn = lambda fn: os.path.join(test_dir, "examples", fn)
+
 
 def test_dodgy_param_sect():
-    fn = os.path.join(test_dir, "examples", "dodgy_param_sect.las")
-    l = las_reader.read(fn)
+    l = las_reader.read(egfn("dodgy_param_sect.las"))
+
+
+def test_mnemonic_good():
+    l = las_reader.read(egfn("mnemonic_good.las"))
+    assert [c.mnemonic for c in l.curves] == [
+        "DEPT", "DT", "RHOB", "NPHI", "SFLU", "SFLA", "ILM", "ILD"]
+
+
+def test_mnemonic_duplicate():
+    l = las_reader.read(egfn("mnemonic_duplicate.las"))
+    assert [c.mnemonic for c in l.curves] == [
+        "DEPT", "DT", "RHOB", "NPHI", "SFLU[0]", "SFLU[1]", "ILM", "ILD"]
+
+
+def test_mnemonic_leading_period():
+    l = las_reader.read(egfn("mnemonic_leading_period.las"))
+    assert [c.mnemonic for c in l.curves] == [
+        "DEPT", "DT", "RHOB", "NPHI", "SFLU", "SFLA", "ILM", "ILD"]
+
+def test_mnemonic_missing():
+    l = las_reader.read(egfn("mnemonic_missing.las"))
+    assert [c.mnemonic for c in l.curves] == [
+        "DEPT", "DT", "RHOB", "NPHI", "UNKNOWN", "SFLA", "ILM", "ILD"]
+
+def test_mnemonic_missing_multiple():
+    l = las_reader.read(egfn("mnemonic_missing_multiple.las"))
+    assert [c.mnemonic for c in l.curves] == [
+        "DEPT", "DT", "RHOB", "NPHI", "UNKNOWN[0]", "UNKNOWN[1]", "ILM", "ILD"]
