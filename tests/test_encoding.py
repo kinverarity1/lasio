3ed850a6f700d251ca32faae25909e1518c7dd5e
Kent Inverarity <kinverarity1@gmail.com>
2015-08-04 17:46:09 +0930

    wrong extension

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -5,16 +5,16 @@ import las_reader
 # These tests seem to be pointless as they always pass... better ideas?
 
 # def test_autodetect_cp437():
-#     fn = r"D:\work\dewnr\logging_software\las_reader\tests\examples\encoding_cp437.py"
+#     fn = r"D:\work\dewnr\logging_software\las_reader\tests\examples\encoding_cp437.las"
 #     l = las_reader.read(fn, autodetect_encoding=True)
 #     assert l._text == codecs.open(fn, mode="r", encoding="cp437").read()
 
 # def test_autodetect_utf8():
-#     fn = r"D:\work\dewnr\logging_software\las_reader\tests\examples\encoding_utf8.py"
+#     fn = r"D:\work\dewnr\logging_software\las_reader\tests\examples\encoding_utf8.las"
 #     l = las_reader.read(fn, autodetect_encoding=True)
 #     assert l._text == codecs.open(fn, mode="r", encoding="utf8").read()
 
 # def test_autodetect_win1252():
-#     fn = r"D:\work\dewnr\logging_software\las_reader\tests\examples\encoding_win1252.py"
+#     fn = r"D:\work\dewnr\logging_software\las_reader\tests\examples\encoding_win1252.las"
 #     l = las_reader.read(fn, autodetect_encoding=True)
 #     assert l._text == codecs.open(fn, mode="r", encoding="win1252").read()
