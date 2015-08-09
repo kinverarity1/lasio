2d583f3871374d2e6757e601263a67fa5f3f5de5
Kent Inverarity <kinverarity1@gmail.com>
2015-08-09 16:13:28 +0930

    fix excel converter - basic test included (see issue #8)

--- lasio/__init__.py
+++ lasio/__init__.py
@@ -1,1 +1,3 @@
 from .las import __version__, read, LASFile, Curve, HeaderItem
+
+from .excel import ExcelConverter
--- lasio/excel.py
+++ lasio/excel.py
@@ -1,8 +1,6 @@
-try:
-    import argparse
-except ImportError:
-    argparse = None
-import sys
+import argparse
+
+import xlwt
 
 from . import las
 
@@ -12,35 +10,10 @@ class ExcelConverter(object):
     def __init__(self, las):
         self.las = las
 
-    def write_excel(self, xlsfn, lib=None):
-        try:
-            import pandas
-            self.write_excel_pandas(self, xlsfn)
-        except ImportError:
-            try:
-                import xlwt
-                self.write_excel_xlwt(self, xlsfn)
-            except ImportError:
-                raise ImportError("Either pandas or xlwt are "
-                                  "required to create Excel files")
-
-    def write_excel_pandas(self):
-        import pandas
-        keys, values = zip(*self.las.metadata.items())
-        md_df = pandas.DataFrame(
-            data=[keys, values], columns=["Mnemonic", "Value"])
-        c_df = pandas.DataFrame(
-            data=[c.data for c in self.las.curves],
-            columns=[c.mnemonic for c in self.las.curves])
-        with pandas.ExcelWriter(xlsfn) as writer:
-            md_df.to_excel(writer, sheet_name='Metadata')
-            c_df.to_excel(writer, sheet_name='Curves')
-
-    def write_excel_xlwt(self):
-        import xlwt
+    def write_excel(self, xlsfn):
         wb = xlwt.Workbook()
-        md_sheet = wb.add_sheet('Metadata')
-        curves_sheet = wb.add_sheet('Curves')
+        md_sheet = wb.add_sheet("Header")
+        curves_sheet = wb.add_sheet("Curves")
 
         md_sheet.write(0, 0, "Mnemonic")
         md_sheet.write(0, 1, "Value")
@@ -49,7 +22,7 @@ class ExcelConverter(object):
             md_sheet.write(i + 1, 1, value)
 
         for i, curve in enumerate(self.las.curves):
-            curves_sheet.write(0, i, curve.name)
+            curves_sheet.write(0, i, curve.mnemonic)
             for j, value in enumerate(curve.data):
                 curves_sheet.write(j + 1, i, value)
 
@@ -60,10 +33,9 @@ def main():
     args = get_parser().parse_args(sys.argv[1:])
     lasfn = args.LAS_filename
     xlsfn = args.Excel_filename
-    lib = args.lib
 
     l = las.LASFile(lasfn)
-    converter = ExcelConverter(l, lib=lib)
+    converter = ExcelConverter(l)
     converter.write_excel(xlsfn)
 
 
@@ -71,8 +43,6 @@ def get_parser():
     parser = argparse.ArgumentParser('Convert LAS file to Excel')
     parser.add_argument('LAS_filename')
     parser.add_argument('Excel_filename')
-    parser.add_argument("-l", "--lib", choices=("pandas", "xlwt"),
-                        help="Python library to use for writing Excel files")
     return parser
 
 
