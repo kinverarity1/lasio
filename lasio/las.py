8698be22d83424f330876940941f4d5a7e3b7f3e
Kent Inverarity <kinverarity@hotmail.com>
2016-01-24 15:28:13 +1030

    get basestring in py3

--- lasio/las.py
+++ lasio/las.py
@@ -40,6 +40,23 @@ except ImportError:
 else:
     from StringIO import StringIO
 
+# get basestring in py3
+
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
 # Required third-party packages available on PyPi:
 
 from namedlist import namedlist
