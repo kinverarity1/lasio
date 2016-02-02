5c131d426186c11ee9ebe741054c74a38aefc285
Kent Inverarity <kinverarity1@users.noreply.github.com>
2016-02-02 18:35:11 +1030

    update and add dependency on setuptools

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
-"""Setup script for lasio
+'''Setup script for lasio'''
 
-Based on:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
+from setuptools import setup
 
-"""
-
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
 from os import path
-
-from distutils.core import setup
 
 from lasio import __version__
 
@@ -20,23 +10,22 @@ from lasio import __version__
 with open(path.join(path.dirname(__file__), "requirements.txt"), "r") as f:
     requirements = f.read().splitlines()
 
-with open(path.join(path.dirname(__file__), "README.rst"), "r") as f:
-    README = f.read()
-
 setup(name='lasio',
-
       version=__version__,
-
       description="Read/write well data from Log ASCII Standard (LAS) files",
-      long_description=README,
-
+      long_description=(
+          'This is a Python 2/3 package to read and write Log ASCII Standard '
+          '(LAS) files, used for borehole data such as geophysical, geological, '
+          'or petrophysical logs. It's compatible with versions 1.2 and 2.0 of '
+          'the LAS file specification, published by the Canadian Well Logging '
+          'Society (http://www.cwls.org/las). In principle it is designed to read '
+          'as many types of LAS files as possible, including ones containing '
+          'common errors or non-compliant formatting.\n\n'
+          'See http://github.com/kinverarity1/lasio for more details.'),
       url="https://github.com/kinverarity1/lasio",
-
       author="Kent Inverarity",
-      author_email="kinverarity1@gmail.com",
-
+      author_email="kinverarity@hotmail.com",
       license="MIT",
-
       classifiers=[
           "Development Status :: 4 - Beta",
           "Environment :: Console",
@@ -52,20 +41,16 @@ setup(name='lasio',
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.2",
           "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
           "Topic :: Scientific/Engineering",
           "Topic :: System :: Filesystems",
           "Topic :: Scientific/Engineering :: Information Analysis",
       ],
-
       keywords="science geophysics io",
-
       packages=["lasio", ],
-
       install_requires=requirements,
-
       entry_points={
           'console_scripts': [
               'las2excel = lasio.excel:main',
