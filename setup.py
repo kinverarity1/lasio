a3ddb6ef00289c5231cd683b45f771e498094d48
Kent Inverarity <kinverarity1@users.noreply.github.com>
2016-02-02 18:37:50 +1030

    typo

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='lasio',
       long_description=(
           'This is a Python 2/3 package to read and write Log ASCII Standard '
           '(LAS) files, used for borehole data such as geophysical, geological, '
-          'or petrophysical logs. It's compatible with versions 1.2 and 2.0 of '
+          'or petrophysical logs. It\'s compatible with versions 1.2 and 2.0 of '
           'the LAS file specification, published by the Canadian Well Logging '
           'Society (http://www.cwls.org/las). In principle it is designed to read '
           'as many types of LAS files as possible, including ones containing '
