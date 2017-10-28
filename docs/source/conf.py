07a666b64388b2ef7de53837c9d9b2d44c4da8bd
Kent Inverarity <kinverarity@hotmail.com>
2017-10-28 13:03:54 +1030

    add basic example to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,8 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
