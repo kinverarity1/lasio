25075412f9c4db24f6190f683b180a30633dd1ee
Kent Inverarity <kinverarity@hotmail.com>
2017-11-04 17:46:06 +1030

    reorganise setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,35 @@ except ImportError:
 
 from lasio import __version__
 
+
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Intended Audience :: Customer Service",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Intended Audience :: End Users/Desktop",
+    "Intended Audience :: Other Audience",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering",
+    "Topic :: System :: Filesystems",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    ]
+
 
 setup(name='lasio',
       version=__version__,
@@ -23,30 +50,7 @@ setup(name='lasio',
       author="Kent Inverarity",
       author_email="kinverarity@hotmail.com",
       license="MIT",
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Environment :: Console",
-          "Intended Audience :: Customer Service",
-          "Intended Audience :: Developers",
-          "Intended Audience :: Education",
-          "Intended Audience :: End Users/Desktop",
-          "Intended Audience :: Other Audience",
-          "Intended Audience :: Science/Research",
-          "License :: OSI Approved :: MIT License",
-          "Natural Language :: English",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.2",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-          "Topic :: Scientific/Engineering",
-          "Topic :: System :: Filesystems",
-          "Topic :: Scientific/Engineering :: Information Analysis",
-      ],
+      classifiers=CLASSIFIERS,
       keywords="science geophysics io",
       packages=["lasio", ],
       install_requires=requirements,
