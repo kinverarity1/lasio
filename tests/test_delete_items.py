c59a6f5588c91d5754835d0a4d1b051df4fc0950
Kent Inverarity <kinverarity@hotmail.com>
2017-12-02 13:42:51 +1030

    extended tests for #155

--- tests/test_delete_items.py
+++ tests/test_delete_items.py
@@ -22,7 +22,12 @@ def test_delete_curve():
     assert len(l.curves.keys()) == (shape - 1)
     assert l.curves.keys() == ['DEPT', 'RHOB', 'NPHI', 'SFLU', 'SFLA','ILM', 'ILD']
 
-def test_delete_section_item():
+def test_delete_section_item_by_index():
+    las = lasio.read(egfn('sample.las'))
+    del las.params[1]
+    assert las.params.keys() == ['BHT', 'FD', 'MATR', 'MDEN', 'RMF', 'DFD']
+
+def test_delete_section_item_by_mnemonic():
     las = lasio.read(egfn('sample.las'))
     del las.params['MDEN']
     assert las.params.keys() == ['BHT', 'BS', 'FD', 'MATR', 'RMF', 'DFD']
