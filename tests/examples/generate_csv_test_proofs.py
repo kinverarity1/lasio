5b10b07bfd910e6693a7c226a3370db39a8aa41c
Kent Inverarity <kinverarity@hotmail.com>
2019-06-30 12:40:25 +0930

    add doctests

--- tests/examples/generate_csv_test_proofs.py
+++ tests/examples/generate_csv_test_proofs.py
@@ -1,11 +1,12 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 import lasio
 
-las = lasio.read('sample.las')
-las.to_csv('sample.las_units-none.csv', units_loc=None)
-las.to_csv('sample.las_units-line.csv', units_loc='line')
-las.to_csv('sample.las_units-parentheses.csv', units_loc='()')
-las.to_csv('sample.las_units-brackets.csv', units_loc='[]')
+# las = lasio.read('sample.las')
+# las.to_csv('sample.las_units-none.csv', units_loc=None)
+# las.to_csv('sample.las_units-line.csv', units_loc='line')
+# las.to_csv('sample.las_units-parentheses.csv', units_loc='()')
+# las.to_csv('sample.las_units-brackets.csv', units_loc='[]')
