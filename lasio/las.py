d3d2f7196d9b01296548c5de93a688a752cc8706
Kent Inverarity <kinverarity@hotmail.com>
2020-05-22 18:31:57 +0930

    fix order of magnitude error 😧

--- lasio/las.py
+++ lasio/las.py
@@ -763,7 +763,7 @@ class LASFile(object):
         elif self._index_unit_contains("F"):
             return self.index * 0.3048
         elif self._index_unit_contains(".1IN"):
-            return (self.index / 12) * 0.3048
+            return (self.index / 120) * 0.3048
         else:
             raise exceptions.LASUnknownUnitError("Unit of depth index not known")
 
@@ -775,7 +775,7 @@ class LASFile(object):
         elif self._index_unit_contains("F"):
             return self.index
         elif self._index_unit_contains(".1IN"):
-            return self.index / 12
+            return self.index / 120
         else:
             raise exceptions.LASUnknownUnitError("Unit of depth index not known")
 
