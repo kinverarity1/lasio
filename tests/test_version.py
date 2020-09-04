faa052a6aa3461b98dd3e1eeecae600451dbd6b2
Kent Inverarity <kinverarity@hotmail.com>
2020-09-04 10:18:56 +0930

    Fix spelling error

--- tests/test_version.py
+++ tests/test_version.py
@@ -13,13 +13,13 @@ def test_verify_default_vcs_tool():
     result = lasio.las_version._get_vcs_version()
     assert version_regex.match(result) 
 
-def test_non_existant_vcs_tool():
+def test_non_existent_vcs_tool():
     version_cmd = ["gt", "describe", "--tags", "--match", "v*"]
     result = lasio.las_version._get_vcs_version(version_cmd)
     assert result == ""
 
 
-def test_explicit_existant_vcs_tool():
+def test_explicit_existent_vcs_tool():
     version_cmd = ["git", "describe", "--tags", "--match", "v*"]
     result = lasio.las_version._get_vcs_version(version_cmd)
     assert version_regex.match(result) 
