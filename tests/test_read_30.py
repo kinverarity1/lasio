ab3c32601c03eae54e263e4af3b9fe7e4ecb9f57
dcslagel <dcs@mailworks.org>
2020-09-08 12:29:43 -0600

    Restore sample_3.0.las test
    -  Restore the sample_3.0.las test for a the most basic read of
    a Las30 file without panicing.
    - Remove the sample_3.0_without_test_sections.las test that was a
      temporary work around for the earlier sample_3.0.las parse panic.

--- tests/test_read_30.py
+++ tests/test_read_30.py
@@ -15,7 +15,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-@pytest.mark.skip(reason="lasio.exception.LASHeaderError in ~Test_Data section")
 def test_read_v30_sample():
     las = lasio.read(stegfn("3.0", "sample_3.0.las"))
     assert las.version[0].mnemonic == "VERS"
@@ -23,11 +22,6 @@ def test_read_v30_sample():
     assert las.version[0].descr == "CWLS LOG ASCII STANDARD -VERSION 3.0"
 
 
-def test_read_v30_sample_without_test_sections():
-    las = lasio.read(stegfn("3.0", "sample_3.0_without_test_sections.las"))
-    assert las.version[0].mnemonic == "VERS"
-    assert las.version[0].value == 3.0
-    assert las.version[0].descr == "CWLS LOG ASCII STANDARD -VERSION 3.0"
 
 
 def test_read_v30_sample_v20_sections_are_empty():
@@ -35,7 +29,7 @@ def test_read_v30_sample_v20_sections_are_empty():
     Verify 'Curves' doesn't read 'Core_*' sections
     Verify 'Parameter' doesn't read 'Performations_*' sections
     """
-    las = lasio.read(stegfn("3.0", "sample_3.0_without_test_sections.las"))
+    las = lasio.read(stegfn("3.0", "sample_3.0.las"))
     assert las.sections['Curves'] == []
     assert las.sections['Parameter'] == []
     assert las.sections['Perforations_Definition'][0].mnemonic == "PERFT:1"
