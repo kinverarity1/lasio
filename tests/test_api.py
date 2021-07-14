c9fa7fc69028b912da249df28dee2e53e7698134
Kent Inverarity <kent.inverarity@sa.gov.au>
2021-07-14 15:09:21 +0930

    Add test for replacing a curve using LASFile.__setitem__

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,6 +8,7 @@ import numpy as np
 import pytest
 
 import lasio
+import lasio.examples
 from lasio import read, las
 
 import logging
@@ -89,3 +90,10 @@ def test_data_attr():
     logger.debug("las.data = {}".format(las.data))
     # the .all() method assumes these are numpy ndarrays; that should be the case.
     assert (las.data == np.asarray([[1, 4, 7], [2, 5, 8], [3, 6, 9]])).all()
+
+def test_replace_curve():
+    las = lasio.examples.open("sample.las")
+    las["NPHI"] = las["NPHI"] * 100
+    assert "NPHI" in las.keys()
+    assert not "NPHI:1" in las.keys()
+    assert not "NPHI:2" in las.keys()
